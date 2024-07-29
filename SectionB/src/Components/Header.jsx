f71161a00ce7e0dcb9749c2b1ed1420e78ae67a1
abidoyejay@gmail.com <abidoyejay@gmail.com>
2024-07-29 11:37:05 +0100

    wip:added section file

---
+++ SectionB/src/Components/Header.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+  
+function Header() {
+    return (
+      <div>
+        <nav className="nav">
+          <div className="nav-left">
+            <a className="brand" href="#">
+                SPA Tasks
+            </a>
+          </div>
+          <div className="nav-right">
+            <div className="tabs">
+              <a href="https://Blockfuse.com">Task Management App by King Sardius</a>
+            </div>
+          </div>
+        </nav>
+      </div>
+    );
+}
+  
+export default Header;
